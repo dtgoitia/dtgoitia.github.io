417bb93858d7f12084ab12c5c017d22ae3d9c5f4
David Torralba Goitia <david.torralba.goitia@gmail.com>
2017-09-04 18:12:33 +0100

    AcademiaBar created

--- app/components/Past.js
+++ app/components/Past.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import NavBar from './NavBar'
 
-class AcademiaEntry extends React.Component {
+class AcademiaBar extends React.Component {
   render() {
+    const l = this.props.length - 10; // Total length - (round corners' X displacement)
+    const dString = 'm 5 0 h '+ l +' a 5 5 0 0 1 0 10 h -'+ l +' a 5 5 0 0 1 0 -10';
     return (
-      <div>AcademiaEntry</div>
+      <svg viewBox={'0 0 '+ this.props.length +' 10'} width={this.props.length}>
+        <path d={dString} fill={this.props.fill} stroke={this.props.stroke} />
+      </svg>
     );
   }
 }
+
+class AcademiaEntry extends React.Component {
+  render() {
+    return (
+      <div>
+        AcademiaEntry
+        <AcademiaBar length='100' fill='white' stroke='black' />
+      </div>
+    );
+  }
+}
+
 class DesktopChronology extends React.Component {
   render() {
     return (
       <div>
         <AcademiaEntry
-          title='Certified Associate in Project Management'
-          subtitle='Project Management Institute (PMI) & Learning People'
+          data={{ title: 'Certified Associate in Project Management',
+            subtitle: 'Project Management Institute (PMI) & Learning People',
+            description: 'This online course is the first step to get involved in project management world, focused to a later PMP, to achieve my goal: become a Project Manager.',
+            start: 'Nov 2014',
+            end: ''
+          }}
         />
       </div>
     );
