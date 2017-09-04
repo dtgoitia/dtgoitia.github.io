18be02b59621633eb35fb4caf93887320b022ebd
David Torralba Goitia <david.torralba.goitia@gmail.com>
2017-09-04 16:38:30 +0100

    Initial page links replaced for <App/> functions

--- app/components/Apps.js
+++ app/components/Apps.js
@@ -2,8 +2,9 @@ import React from 'react';
 import Intro from './intro';
 import Language from './language';
 
+
 const db = {
-  'languageBar': [
+  languageBar: [
     ['Spanish', 'idioma'],
     ['English', 'language']
   ],
@@ -11,8 +12,44 @@ const db = {
     ['Spanish', 'CREATIVO · DINÁMICO · COMPROMETIDO · CURIOSO'],
     ['English', 'CREATIVE · DYNAMIC · COMMITTED · INQUISITIVE']
   ],
+  personalInfo: {
+    name: 'David',
+    surnames: 'Torralba Goitia',
+    phone: '+44 (0)7598 178 122',
+    email: 'david.torralba.goitia@gmail.com',
+    location: [
+      ['Spanish', 'Oxford, Reino Unido'],
+      ['English', 'Oxford, United Kingdom']
+    ]
+  },
+  socialMedia: {
+    github:   'https://github.com/dtgoitia',
+    linkedin: 'https://www.linkedin.com/in/dtgoitia',
+    facebook: 'https://www.facebook.com/dtgoitia'
+  },
+  academia: [
+    {
+      h1: 'Secundary School',
+      h2: 'J. M. Barandiaran (behekoa)',
+      p:  '4 years during which I had every detail about my life\'s path perfectly defined and I was absolutelly sure about my future... (naive)',
+      /**
+       * Find out how to show dates on different languages, this way you can
+       * avoid to create an array with dates in each language stored as strings.
+       * The fact of storing them as dates allows you to sort them if needed.
+       */ 
+      t0: 'Sep 2002',
+      t1: 'Jun 2006'
+    },
+    {
+      h1: 'Science High School',
+      h2: 'J. M. Barandiaran (goikoa)',
+      p:  '2 years of non-obligatory education, in which I had to start defining my future studies, focusing on science areas.',
+      t0: 'Sep 2006',
+      t1: 'Jun 2008'
+    }
+  ]
 }
- 
+
 class App extends React.Component {
   constructor (props) {
     super(props)
@@ -42,21 +79,35 @@ class App extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <Intro
-          db={db}
-          st={this.state}
-          lang={this.state.language}
-        />
-        <Language
-          db={db}
-          st={this.state}
-          lang={this.state.language}
-          changeLanguage={this.changeLanguage}
-        />
-      </div>
-    )
+    const currentPage = this.state.page
+    switch (this.state.page) {
+      case 'intro':
+        return (
+          <div>
+            <Intro
+              db={db}
+              st={this.state}
+              lang={this.state.language}
+              changePage={this.changePage}
+            />
+            <Language
+              db={db}
+              st={this.state}
+              lang={this.state.language}
+              changeLanguage={this.changeLanguage}
+            />
+          </div>
+        );
+        break;
+    
+      default:
+        return(
+          <div>
+            Oops, this.state.page = {currentPage ? currentPage : 'null'}
+          </div>
+        );
+        break;
+    }
   }
 }
  
--- app/components/Intro.js
+++ app/components/Intro.js
@@ -1,16 +1,102 @@
 import React from 'react';
+class SvgMenuLeft extends React.Component {
+  render () {
+    return (
+      <svg width="265" height="350">
+        <path
+          d="m 215 200 v -145 a 7.5 7.5 0 0 1 50 0 v 145 l -89 150 h -176"
+          fill="rgba(150,150,150,0.78)"
+        />
+      </svg>
+    )
+  }
+}
+
+class SvgMenuCenter extends React.Component {
+  render () {
+    return (
+      <svg width="250" height="350">
+        <path
+          d="
+            m 63 200
+            v -170
+            a 7.5 7.5 0 0 1 50 0
+            v 170
+            l 63 150
+            h -176
+          "
+          fill="rgba(206,206,206,0.78)"
+        />
+      </svg>
+    )
+  }
+}
+
+class SvgMenuRight extends React.Component {
+  render () {
+    return (
+      <svg width="265" height="350">
+        <path
+          d="
+            m 0 200
+            v -152
+            a 7.5 7.5 0 0 1 50 0
+            v 152
+            l 215 150
+            h -176
+          "
+          fill="rgba(255,255,255,0.78)"
+        />
+      </svg>
+    )
+  }
+}
+
+class MenuVerticalText extends React.Component {
+  render () {
+    return (
+      <div className='caja_girada'>
+        <span onClick={this.props.changePage.bind(null, this.props.words[1])}>
+          <span className='texto_oculto'>{this.props.words[0]} </span>
+          <span className='texto_fijo'>{this.props.words[1]}</span>
+        </span>
+      </div>
+    )
+  }
+}
+
+class SvgMenu extends React.Component {
+  render () {
+    return (
+      <div className='menu'>
+        <div className='vertical1'>
+          <SvgMenuLeft />
+          <MenuVerticalText words={['my', 'past']} changePage={this.props.changePage}/>
+        </div>
+        <div className='vertical2'>
+          <SvgMenuCenter />
+          <MenuVerticalText words={['my', 'present']} changePage={this.props.changePage}/>
+        </div>
+        <div className='vertical3'>
+          <SvgMenuRight />
+          <MenuVerticalText words={['your', 'future']} changePage={this.props.changePage}/>
+        </div>
+      </div>
+    )
+  }
+}
 
 class Intro extends React.Component {
   render() {
     return (
       <div>
-        <div className='nombre'>
-          david
+        <div className='nombre'>david
           <span className='apellido'>torralba</span>
         </div>
         <div className='web2'>
           { this.props.db.introSubtitle.filter( arr => arr[0] === this.props.st.language )[0][1]}
         </div>
+        <SvgMenu changePage={this.props.changePage} />
       </div>
     )
   }
