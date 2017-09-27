293dd0ddea62f2ebf16183264d31fd999352cc67
David Torralba Goitia <david.torralba.goitia@gmail.com>
2017-09-27 06:49:05 +0100

    Past component updated to latest pastEntry library workflow

--- app/components/Past.js
+++ app/components/Past.js
@@ -183,38 +183,13 @@ const test = [
 class Past extends React.Component {
   render () {
     // Convert to Date all string-dates in db
-    let db = {};
-    db.academia   = originalDb.academia.map(entry => pastEntry.convertEntryStringDatestoDates(entry));
-    db.experience = originalDb.experience.map(entry => pastEntry.convertEntryStringDatestoDates(entry));
+    const db = pastEntry.importDb(originalDb);
 
-    // Gather all entries in a single array
-    const allEntries = originalDb.academia.concat(originalDb.experience)
-    .map(entry => pastEntry.convertEntryStringDatestoDates(entry));
-
-    // Get earliest start date from all entries
-    const earliestDate = allEntries.reduce( (previousEntry, currentEntry) => {
-      if (previousEntry.start < currentEntry.start) {
-        return previousEntry
-      } else {
-        return currentEntry
-      }
-    }).start;
-
-    // Get latest end date from all entries
-    const latestDate = allEntries.reduce( (previousEntry, currentEntry) => {
-      if (previousEntry.end > currentEntry.end) {
-        return previousEntry
-      } else {
-        return currentEntry
-      }
-    }).end;
-
-    // Get range of index units covered by the database (db)
-    const totalAmountOfUnits = pastEntry.getRelativeIndex(earliestDate, latestDate);
-
-    // console.log('case 1:', pastEntry.getEntryBarIndexes(db.academia[0], earliestDate));
-    // console.log('case 2:', pastEntry.getEntryBarIndexes(db.academia[1], earliestDate));
-    // console.log('case 3:', pastEntry.getEntryBarIndexes(db.academia[3], earliestDate));
+    // Get db range
+    const dbRange = pastEntry.getDbRange(db);
+    
+    // Get database bar list sorted and grouped by index
+    let dbBars = pastEntry.getDbBars(db);
 
     return (
       <div>
