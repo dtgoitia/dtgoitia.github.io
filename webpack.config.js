701812ae57ae28850099ac73758ff8faa10fec72
David Torralba Goitia <david.torralba.goitia@gmail.com>
2017-10-14 09:02:49 +0100

    Font and URL loaders added to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,10 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.(js)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      { test: /\.(json)$/, use: 'json-loader' }
+      { test: /\.(js)$/,      use: 'babel-loader' },
+      { test: /\.css$/,       use: ['style-loader', 'css-loader'] },
+      { test: /\.(json)$/,    use: 'json-loader' },
+      { test: /\.(png|jpg)$/, use: 'url-loader' }
     ]
   },
   plugins: [new HtmlWebPackPlugin({
