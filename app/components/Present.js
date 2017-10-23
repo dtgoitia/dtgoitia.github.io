deff850206009d42be5606e0c26eec90f3d9289f
David Torralba Goitia <david.torralba.goitia@gmail.com>
2017-10-23 06:44:35 +0100

    Present page progressed (DUE)
    Scafold present basic structure. Starting to set styles and passing
    data to components.

--- app/components/Present.js
+++ app/components/Present.js
@@ -1,6 +1,39 @@
 import React from 'react';
 import NavBar from './NavBar';
 
+const PresentEntryGraph = props => {
+  return <div className='presentEntryGraph'>{props.graph}</div>;
+};
+
+const PresentEntryText = props => {
+  return <div className='presentEntryText'>{props.text}</div>;
+};
+
+const PresentEntry = props => {
+  return(
+    <div className='presentEntry'>
+      <PresentEntryGraph graph={props.content.graph} />
+      <PresentEntryText text={props.content.text}/>
+    </div>
+  );
+};
+
+const lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+const contentArray = [
+  {
+    text: lorem,
+    graph: 'graphURL!!'
+  },
+  {
+    text: lorem,
+    graph: 'graphURL!!'
+  },
+  {
+    text: lorem,
+    graph: 'graphURL!!'
+  }
+];
+
 class Present extends React.Component {
   render () {
     return (
@@ -10,7 +43,11 @@ class Present extends React.Component {
           right={{label:'YOUR FUTURE',target:'future'}}
           changePage={this.props.changePage}
         />
-        Hi! I am "Present.js"
+        <div className='presentEntryContainer'>
+          {contentArray.map((content, i) => {
+            return <PresentEntry content={content} key={i}/>;
+          })}
+        </div>
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     ]
   },
   devServer: {
-    host: '0.0.0.0',
+    // host: '0.0.0.0',
     port: 8080,
     stats: 'errors-only'
   },
