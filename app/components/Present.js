d7ca961c3e2fb3a5a3c0d8e7e849553f2394783d
David Torralba Goitia <david.torralba.goitia@gmail.com>
2017-10-25 16:23:37 +0100

    Add more logos
    - Finish JavaScript component logo
    - Add React component logo
    - Add AutoCAD component logo
    - Add Node component logo

--- app/components/Present.js
+++ app/components/Present.js
@@ -3,6 +3,9 @@ import NavBar from './NavBar';
 import LogoHtml5 from './logos/html5';
 import LogoCss3 from './logos/css3';
 import LogoJs from './logos/js';
+import LogoNode from './logos/node';
+import LogoAutoCAD from './logos/acad';
+import LogoReact from './logos/react';
 
 const PresentSkills = props => {
   return(
@@ -10,20 +13,29 @@ const PresentSkills = props => {
       <div className='presentTitle'>Title!</div>
       {
         props.skillArray.map((skill, i) => {
-          return <PresentSkill skill={skill} key={i}/>;
+          return <PresentSkill skill={skill} key={i} color={props.color}/>;
         })
       }
     </div>
-    
   );
 };
 
 const PresentSkill = props => {
+  let graph;
+  switch (props.skill.graph) {
+  case 'react':
+    graph = <LogoReact color={props.color} />;
+    break;
+  default:
+    graph = props.skill.graph;
+    break;
+  }
+
   return(
     <div className='presentEntry'>
       <div className='presentEntryTitle'>{props.skill.skill}</div>
       <div className='presentEntryText'>{props.skill.text}</div>
-      <div className='presentEntryGraph'>{props.skill.graph}</div>
+      <div className='presentEntryGraph'>{graph}</div>
     </div>
   );
 };
@@ -31,9 +43,9 @@ const PresentSkill = props => {
 const lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 const skillArray = [
   {
-    skill: 'skill name!',
+    skill: 'React',
     text: lorem,
-    graph: 'graphURL!!'
+    graph: 'react'
   },
   {
     skill: 'skill name!',
@@ -41,7 +53,7 @@ const skillArray = [
     graph: 'graphURL!!'
   },
   {
-    skill: 'skill name!',
+    skill: 'React',
     text: lorem,
     graph: 'graphURL!!'
   }
@@ -56,10 +68,7 @@ class Present extends React.Component {
           right={{label:'YOUR FUTURE',target:'future'}}
           changePage={this.props.changePage}
         />
-        <LogoHtml5 color={'white'} />
-        <LogoCss3 color={'white'} />
-        <LogoJs color={'white'} />
-        <PresentSkills skillArray={skillArray} />
+        <PresentSkills skillArray={skillArray} color={'white'}/>
       </div>
     );
   }
